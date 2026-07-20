d98030e636b0e95923135f4d2298bd6beb11e972
JPCD-hub <smitecufino@gmail.com>
2026-07-20 13:03:33 -0500

    Align courtesy ticket stamps and fix art sizing

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE_PREFIX = 'live-vive-el-arte-public-';
-const CACHE = `${CACHE_PREFIX}v6`;
+const CACHE = `${CACHE_PREFIX}v7`;
 const LEGACY_CACHES = ['live-public-shell-v1'];
-const SHELL = ['./', './index.html', './public.css?v=6', './public.js?v=4', './assets/icon.svg', './assets/social-live.svg', './Boleta%202.jpeg', './boleta%201.jpeg'];
+const SHELL = ['./', './index.html', './public.css?v=7', './public.js?v=4', './assets/icon.svg', './assets/social-live.svg', './Boleta%202.jpeg', './boleta%201.jpeg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting()));
